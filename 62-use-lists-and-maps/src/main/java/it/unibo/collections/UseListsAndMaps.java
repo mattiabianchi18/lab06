5b1f52d88cee943d4ff8361cbe3e916457664056
Mattia Bianchi <mattia.bianchi18@studio.unibo.it>
2025-11-06 13:22:21 +0100

    Completato esercizio 2

--- 62-use-lists-and-maps/src/main/java/it/unibo/collections/UseListsAndMaps.java
+++ 62-use-lists-and-maps/src/main/java/it/unibo/collections/UseListsAndMaps.java
@@ -1,13 +1,22 @@
 package it.unibo.collections;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
+import java.util.TreeSet;
+import java.util.concurrent.TimeUnit;
 
 /**
  * Example class using {@link List} and {@link Map}.
  *
  */
 public final class UseListsAndMaps {
+
+    private static final int ELEMS = 100_000;
+    private static final int READS = 1_000;
 
     private UseListsAndMaps() {
     }
@@ -17,54 +26,64 @@ public final class UseListsAndMaps {
      *            unused
      */
     public static void main(final String... s) {
-        /*
-         * 1) Create a new ArrayList<Integer>, and populate it with the numbers
-         * from 1000 (included) to 2000 (excluded).
-         */
-        /*
-         * 2) Create a new LinkedList<Integer> and, in a single line of code
-         * without using any looping construct (for, while), populate it with
-         * the same contents of the list of point 1.
-         */
-        /*
-         * 3) Using "set" and "get" and "size" methods, swap the first and last
-         * element of the first list. You can not use any "magic number".
-         * (Suggestion: use a temporary variable)
-         */
-        /*
-         * 4) Using a single for-each, print the contents of the arraylist.
-         */
-        /*
-         * 5) Measure the performance of inserting new elements in the head of
-         * the collection: measure the time required to add 100.000 elements as
-         * first element of the collection for both ArrayList and LinkedList,
-         * using the previous lists. In order to measure times, use as example
-         * TestPerformance.java.
-         */
-        /*
-         * 6) Measure the performance of reading 1000 times an element whose
-         * position is in the middle of the collection for both ArrayList and
-         * LinkedList, using the collections of point 5. In order to measure
-         * times, use as example TestPerformance.java.
-         */
-        /*
-         * 7) Build a new Map that associates to each continent's name its
-         * population:
-         *
-         * Africa -> 1,110,635,000
-         *
-         * Americas -> 972,005,000
-         *
-         * Antarctica -> 0
-         *
-         * Asia -> 4,298,723,000
-         *
-         * Europe -> 742,452,000
-         *
-         * Oceania -> 38,304,000
-         */
-        /*
-         * 8) Compute the population of the world
-         */
+
+        final List<Integer> arrayList = new ArrayList<>();
+        for(int i = 1000; i<2000; i++) {
+            arrayList.add(i);
+        }
+        
+        final List<Integer> linkedList = new LinkedList<>(arrayList);
+        
+        final int temp = arrayList.get(0);
+        arrayList.set(0, arrayList.get(arrayList.size() - 1));
+        arrayList.set(arrayList.size() -1, temp);
+
+        for(final int elem : arrayList) {
+            System.out.println(elem);
+        }
+        
+        long time = System.nanoTime();
+        for (int i = 0; i < ELEMS; i++) {
+            arrayList.add(0, i);
+        }
+        time = System.nanoTime() - time;
+        final var millis = TimeUnit.NANOSECONDS.toMillis(time);
+        System.out.println(
+            "ArrayList: inserting " + ELEMS + " elements at the head took  "
+            + time + "ns (" + millis + "ms)"
+        );
+
+        time = System.nanoTime();
+        for (int i = 0; i < ELEMS; i++) {
+            linkedList.add(0, i);
+        }
+        time = System.nanoTime() - time;
+        final var millisLinked = TimeUnit.NANOSECONDS.toMillis(time);
+        System.out.println(
+            "LinkedList: inserting " + ELEMS + " elements at the head took  "
+            + time + "ns (" + millisLinked + "ms)"
+        );
+        
+        final int middlePosition = arrayList.size() / 2;
+        time = System.nanoTime();
+        for(int i = 0; i < READS; i++) {
+            arrayList.get(middlePosition);
+        }
+        time = System.nanoTime() - time;
+        System.out.println("ArrayList: ... " + time + "ns");
+
+        final Map<String, Long> worldPopulation = new HashMap<>();
+        worldPopulation.put("Africa", 1_110_635_000L);
+        worldPopulation.put("Americas", 972_005_000L);
+        worldPopulation.put("Antartica", 0L);
+        worldPopulation.put("Asia", 4_298_723_000L);
+        worldPopulation.put("Europe", 742_452_000L);
+        worldPopulation.put("Oceania",38_304_000L);
+
+        long totalPopulation = 0;
+        for(final long population : worldPopulation.values()){
+            totalPopulation += population;
+        }
+        System.out.println("World population: " + totalPopulation);
     }
 }
